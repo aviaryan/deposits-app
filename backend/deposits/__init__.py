80c60063b6c0773938a72e9a8d488b0947775b03
Avi Aryan <avi.aryan123@gmail.com>
2018-03-10 00:59:17 +0530

    enable cors in backend

--- backend/deposits/__init__.py
+++ backend/deposits/__init__.py
@@ -3,9 +3,11 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(os.environ.get('CONFIG', 'config.LocalConfig'))
 
 # Database
