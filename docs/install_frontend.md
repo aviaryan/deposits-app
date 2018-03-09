9cfe757c4de4a95585b58d76519383e7f0beb552
Avi Aryan <avi.aryan123@gmail.com>
2018-03-09 20:21:50 +0530

    use semantic UI

---
+++ docs/install_frontend.md
@@ -0,0 +1,8 @@
+# Setting Up
+
+```sh
+cd frontend
+yarn install
+yarn start
+```
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Menu, Container } from 'semantic-ui-react';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-					Done.
-        </p>
-      </div>
+			<Container>
+				<Menu>
+					<Menu.Header content="Deposits"/>
+				</Menu>
+			</Container>
     );
   }
 }
