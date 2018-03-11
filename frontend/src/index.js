5307eae765702a2a876b48c0af79b14b9a49e19c
Avi Aryan <avi.aryan123@gmail.com>
2018-03-11 10:20:47 +0530

    use redux-persist to store state in localStorage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,10 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import depositsApp from './reducers/index';
 import { Provider } from 'react-redux';
-
+// persist
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 // uikit
 import 'uikit/dist/css/uikit.min.css';
 import Icons from 'uikit/dist/js/uikit-icons';
@@ -15,12 +18,22 @@ import UIkit from 'uikit';
 
 UIkit.use(Icons);
 
-let store = createStore(depositsApp);
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, depositsApp)
+
+let store = createStore(persistedReducer)
+let persistor = persistStore(store)
 
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
 );
